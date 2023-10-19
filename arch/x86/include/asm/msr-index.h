87b3a76d797acd4422dc52159ca877517f2c597e
Chang S. Bae <chang.seok.bae@intel.com>
2023-10-19 16:54:58 -0700

    x86/msr-index: Define MSR index and bit for the microcode staging feature
    The microcode staging feature involves two key MSR entities, the
    presence of which is indicated by bit 16 of IA32_ARCH_CAPABILITIES:
    
    * Bit 4 in IA32_MCU_ENUMERATION shows the availability of the microcode
      staging feature.
    
    * Staging is managed through MMIO registers, with
      IA32_MCU_STAGING_MBOX_ADDR MSR specifying the physical address of the
      first MMIO register.
    
    Define the MSR index and bit assignments, helping the upcoming staging
    code to make use of the hardware feature.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -164,6 +164,10 @@
 						 * Processor MMIO stale data
 						 * vulnerabilities.
 						 */
+#define ARCH_CAP_MCU_ENUM		BIT(16) /*
+						 * Indicates the presence of microcode update
+						 * feature enumeration and status information
+						 */
 #define ARCH_CAP_FB_CLEAR		BIT(17)	/*
 						 * VERW clears CPU fill buffer
 						 * even on MDS_NO CPUs.
@@ -883,6 +887,11 @@
 
 #define MSR_IA32_UCODE_WRITE		0x00000079
 #define MSR_IA32_UCODE_REV		0x0000008b
+
+#define MSR_IA32_MCU_ENUMERATION	0x0000007b
+#define MCU_STAGING			BIT(4)
+
+#define MSR_IA32_MCU_STAGING_MBOX_ADDR	0x000007a5
 
 /* Intel SGX Launch Enclave Public Key Hash MSRs */
 #define MSR_IA32_SGXLEPUBKEYHASH0	0x0000008C
