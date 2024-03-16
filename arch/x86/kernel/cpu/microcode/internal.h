c09ab0c259456e5899f8f72ddcfb5c78d93fdc0f
Chang S. Bae <chang.seok.bae@intel.com>
2024-03-15 23:46:04 -0700

    x86/microcode/intel_staging: Implement staging logic
    The staging firmware operates through a protocol via the MMIO interface.
    The protocol defines a serialized sequence that begins by clearing the
    hardware with an abort request. It then proceeds through iterative
    process of sending data, initiating transactions, waiting for processing,
    and reading responses.
    
    To facilitate this interaction, follow the outlined protocol. Refactor
    the waiting code to manage loop breaks more effectively. Data transfer
    involves a next level of detail to handle the mailbox format. While
    defining helpers, leave them empty for now.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    ---
    RFC-V1 -> V1: Rename the function name and change the return type.

--- arch/x86/kernel/cpu/microcode/internal.h
+++ arch/x86/kernel/cpu/microcode/internal.h
@@ -120,11 +120,7 @@ void load_ucode_intel_bsp(struct early_load_data *ed);
 void load_ucode_intel_ap(void);
 void reload_ucode_intel(void);
 struct microcode_ops *init_intel_microcode(void);
-static inline enum ucode_state do_stage(u64 pa, void *ucode_ptr, unsigned int totalsize)
-{
-	pr_debug_once("Need to implement the staging code.\n");
-	return UCODE_ERROR;
-}
+enum ucode_state do_stage(u64 pa, void *ucode_ptr, unsigned int totalsize);
 #else /* CONFIG_CPU_SUP_INTEL */
 static inline void load_ucode_intel_bsp(struct early_load_data *ed) { }
 static inline void load_ucode_intel_ap(void) { }
